f191eb0b8803ce9d7726927a05f8ea9f6326d8ac
ConorobDIT <c14733295@mydit.ie>
2017-04-06 05:49:41 +0100

    forgot to add excludes

--- backupcron.sh
+++ backupcron.sh
@@ -7,4 +7,4 @@ FILENAME=backup-$TIME.tar.gz     # Setting file name
 LOGNAME=backup-$TIME.log		 # Creating log file
 DESDIR=/home/network/backup                   # Destination of backup file.
 
-tar -cvpzf $DESDIR/$FILENAME /  > $LOGNAME  #Backup Command
+tar -cvpzf $DESDIR/$FILENAME --exclude == '/dev/*' --exclude '/proc/*' --exclude '/sys/*' --exclude '/tmp/*' --exclude '/run/*' --exclude '/mnt/*' --exclude '/media/*' --exclude '/lost+found'  /  > $LOGNAME  #Backup Command
